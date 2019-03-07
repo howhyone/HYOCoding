8a7b6bc6af1a085006a8bd710f6583e26dc57aa1
mob <zx14993@163.com>
2019-03-07 19:07:31 +0800

    Initial Commit

---
+++ HYOCoding/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HYOCoding
+//
+//  Created by mob on 2019/3/7.
+//  Copyright © 2019年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
